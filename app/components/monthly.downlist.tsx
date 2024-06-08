0dfdca25e947945a0e755718d02819a5a767e9b4
ge11123 <as8354093@gmail.com>
2024-06-08 22:12:05 +0800

    feat: 調整月份下拉選單樣式

--- app/components/monthly.downlist.tsx
+++ app/components/monthly.downlist.tsx
@@ -4,10 +4,14 @@ const MONTHLY = [
     "一月", "二月", "三月", "四月", "五月", "六月", "七月", "八月", "九月", "十月", "十一月", "十二月"
 ]
 
+interface DownListProps {
+    className?: string;
+}
 
-const Downlist: React.FC = () => {
+
+const Downlist: React.FC<DownListProps> = ({ className }) => {
     return (
-        <div className="monthlyDownlist__container">
+        <div className={className}>
             <select className="monthlyDownlist__body">
                 {MONTHLY.map((month, index) => (
                     <option key={index} value={index + 1} className="monthlyDownlist____option">{month}</option>
