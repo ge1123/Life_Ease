e6d4078b4bd58eadb29691df7ca33587c3308181
ge11123 <as8354093@gmail.com>
2024-06-20 11:39:33 +0800

    refactor: 調整 monthlyTasks 結構

--- app/monthlytasks/components/monthlyTasks.tsx
+++ app/monthlytasks/components/monthlyTasks.tsx
@@ -13,35 +13,48 @@ export default function MonthlyTasks() {
         selectedMonth,
         monthlyTasks } = useMonthlyTasksState();
 
+
+    const chooseMonth = (
+        <>
+            <span className='monthlyTask__label'>請選擇月份</span>
+            <div className="monthlyTask__dropDown-container">
+                <DownList
+                    className="monthlyTask__monthlyDownList"
+                    selectedMonth={selectedMonth}
+                    setSelectedMonth={setSelectedMonth}
+                    monthsDict={MONTHS_DICT}
+                />
+            </div>
+        </>
+    )
+
+    const tasks = (
+        <>
+            {monthlyTasks.length === 0 ? (
+                <div>無工作事項</div>
+            ) : (
+                monthlyTasks.map((task, index) => (
+                    <div key={index} className="monthlyTask__task">
+                        {/* 渲染任務的詳細信息 */}
+                        <span>{task.dueDate ? task.dueDate : '無日期'}</span> {/* 格式化日期 */}
+                        <h3>{task.title}</h3>
+                        <p>{task.description}</p>
+                    </div>
+                ))
+            )}
+        </>
+    )
+
+
     return (
         <MainLayout>
             <div className="monthlyTask__container">
-                <span className='monthlyTask__label'>請選擇月份</span>
-                <div className="monthlyTask__dropDown-container">
-                    <DownList
-                        className="monthlyTask__monthlyDownList"
-                        selectedMonth={selectedMonth}
-                        setSelectedMonth={setSelectedMonth}
-                        monthsDict={MONTHS_DICT}
-                    />
-                </div>
+                {chooseMonth}
             </div>
 
             <div className="monthlyTask__tasks-container">
-                {monthlyTasks.length === 0 ? (
-                    <div>無工作事項</div>
-                ) : (
-                    monthlyTasks.map((task, index) => (
-                        <div key={index} className="monthlyTask__task">
-                            {/* 渲染任務的詳細信息 */}
-                            <span>{task.dueDate ? task.dueDate : '無日期'}</span> {/* 格式化日期 */}
-                            <h3>{task.title}</h3>
-                            <p>{task.description}</p>
-                        </div>
-                    ))
-                )}
+                {tasks}
             </div>
-
         </MainLayout>
     );
 }
